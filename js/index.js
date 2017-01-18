37e59ef8ff658b83fe21a59f30d11e73b8bf4f79
moeamaya <moeamaya@gmail.com>
2017-01-18 13:31:06 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+
+(function(){
+
+
+
+
+  
+
+
+  // Helpers ------------------------------------
+
+  function removeActive(elements) {
+    elements.forEach(function(el) { el.classList.remove('is-active'); });
+  }
+
+  function addActive(elements) {
+    elements.forEach(function(el) { el.classList.add('is-active'); });
+  }
+
+
+})();
---
+++ publications.md
@@ -0,0 +1,9 @@
+---
+title: News and Publications
+header_title: News & Publications
+header_subtitle: CYTOVALE
+header_image: "/uploads/banner4.png"
+layout: blog
+---
+
+# News and Publications
