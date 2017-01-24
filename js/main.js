b418205edd86801ea4c2f9de6e290c991980fe1c
moeamaya <moeamaya@gmail.com>
2017-01-23 22:21:59 -0800

    added basic animation to homepage header

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 (function(){
 
 
+  // var zooms = document.querySelectorAll('.header-index__zoom-style, .header-index__zoom-mask' )
+  var zooms = document.querySelectorAll('.header-index__zoom-mask' );
 
+
+  var updown = document.querySelectorAll('.header-index__zoom-line.top, .header-index__zoom-line.bottom');
+  var lines = document.querySelectorAll('.header-index__zoom-line .box');
+
+
+  animate({
+    el: '.header-index__zoom',
+    // opacity: [0, 1],
+    rotate: [45, 0],
+    scale: [0.8, 1],
+    duration: 600,
+    delay: 1600,
+    easing: 'easeOutQuad'
+  })
+
+  animate({
+    el: zooms,
+    opacity: [0, 1],
+    duration: 600,
+    delay: 2000,
+    easing: 'easeOutQuad'
+  })
+
+  animate({
+    el: lines,
+    opacity: [1, 0.1],
+    scaleX: [1, 0.2],
+    scaleY: [1, 300],
+    duration: 1000,
+    delay: 2400,
+    easing: 'easeOutQuad'
+  })
 
 
   // -----------------------------------
