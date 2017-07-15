a2010666c6369905b0cf6425930e366861c061b3
moeamaya <moeamaya@gmail.com>
2017-07-15 16:15:12 -0700

    added simple animation to homepage

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 
 (function(){
 
+  var teamContainer = document.querySelector('.js-team');
   var team = document.querySelector('.index-team__employees');
+  
+  var width = 0;
+  var duration = 25 * width;
+  var state = 'fwd'; // rev
+
 
   var dupes = [];
   var employees = [].slice.call(document.querySelectorAll('.index-team__employee'));
@@ -17,15 +23,95 @@
   shuffle(dupes);
 
   dupes.forEach(function(dupe, i){
-    if (i % 3){
-      dupe.style.transform = 'scale('+ randomRange(1.0, 1.0) +') translateX('+ i * 80 +'px) translateY('+ 260 +'px)'
-    } else if (i % 2) {
-      dupe.style.transform = 'scale('+ randomRange(1.0, 1.0) +') translateX('+ i * 80 +'px) translateY('+ 130 +'px)'
+    if (i % 3 === 0) {
+      dupe.style.transform = 'scale('+ randomRange(1.0, 1.0) +') translateX('+ (i) * 60 +'px) translateY('+ 130 +'px)';
+    } else if (i % 3 === 1){
+      dupe.style.transform = 'scale('+ randomRange(1.0, 1.0) +') translateX('+ (i) * 60 +'px) translateY('+ 260 +'px)';
+    } else {
+      dupe.style.transform = 'scale('+ randomRange(1.0, 1.0) +') translateX('+ (i) * 60 +'px) translateY('+ 0 +'px)'
     }
-    // dupe.style.transform = 'scale('+ randomRange(0.6, 1.2) +') translateX('+ randomRange(-15, 15) +'px)';
+
+    if (i === dupes.length - 1) {
+      width = i * 60 + 180 - window.innerWidth;
+      duration =  width * 15;
+      console.log('Duration: ' + duration);
+    }
+
+
     team.appendChild(dupe);
-  });
+    dupe.addEventListener('mouseover', function() {
+      animate.stop(teamContainer);
+    });
+    dupe.addEventListener('mouseout', function() {
+      var xTransform = teamContainer.style.transform;
+      var xPos = parseNumber(xTransform);
+        
+      var dir;
+      var func;
+      var tempDuration;
+
+      if (state === 'fwd') {
+        dir = -width;
+        func = reverse;
+        var percent = Math.abs(xPos / width);
+        tempDuration = (1 - percent) * duration;
+      } else {
+        dir = 0;
+        func = forward;
+        var percent = Math.abs(xPos / width);
+        tempDuration = (percent) * duration;
+      }
   
+
+
+      animate({
+        el: teamContainer,
+        translateX: [xPos, dir],
+        easing: 'linear',
+        duration: tempDuration,
+        complete: func
+      });
+
+    })
+  });
+
+
+  function forward() {
+    state = 'fwd'
+    animate({
+      el: teamContainer,
+      translateX: [0, -width],
+      easing: 'linear',
+      duration: duration,
+      complete: reverse
+    }); 
+  }
+  
+  function reverse() {
+    state = 'rev'
+    animate({
+      el: teamContainer,
+      translateX: [-width, 0],
+      easing: 'linear',
+      duration: duration,
+      complete: forward
+    }); 
+  }
+
+  setTimeout(function(){
+    state = 'fwd'
+    animate({
+      el: teamContainer,
+      translateX: [0, -width],
+      easing: 'linear',
+      duration: duration,
+      complete: reverse
+    }); 
+  }, 800);
+
+
+
+
 
 
 
@@ -60,6 +146,12 @@
 
   function addActive(elements) {
     elements.forEach(function(el) { el.classList.add('is-active'); });
+  }
+
+  function parseNumber(str) {
+    var regex = /[+-]?\d+(\.\d+)?/g;
+    var floats = str.match(regex).map(function(v) { return parseFloat(v); });
+    return floats[0];
   }
 
 
